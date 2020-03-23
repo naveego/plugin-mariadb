fd7a480726bed1de122c10134e2949a636a5bccb
Wyatt Roehler <wyatt.roehler@naveego.com>
2020-03-23 15:32:24 -0400

    chore(branding): updated text on replication config to say MariaDB

--- PluginMariaDB/API/Replication/GetSchemaJson.cs
+++ PluginMariaDB/API/Replication/GetSchemaJson.cs
@@ -16,19 +16,19 @@ namespace PluginMariaDB.API.Replication
                     {
                         {"type", "string"},
                         {"title", "Schema Name"},
-                        {"description", "Name of schema to put golden and version tables into in MySQL"},
+                        {"description", "Name of schema to put golden and version tables into in MariaDB"},
                     }},
                     {"GoldenTableName", new Dictionary<string, string>
                     {
                         {"type", "string"},
                         {"title", "Golden Record Table Name"},
-                        {"description", "Name for your golden record table in MySQL"},
+                        {"description", "Name for your golden record table in MariaDB"},
                     }},
                     {"VersionTableName", new Dictionary<string, string>
                     {
                         {"type", "string"},
                         {"title", "Version Record Bucket Name"},
-                        {"description", "Name for your version record table in MySQL"},
+                        {"description", "Name for your version record table in MariaDB"},
                     }},
                 }},
                 {"required", new []
